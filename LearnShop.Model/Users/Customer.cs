0970e30dd68f9bc439ebd7a9408ff4b7faaec797
Samuel Costa <samueljf18@hotmail.com>
2025-06-28 15:26:34 -0300

    Adicionando propriedades PhoneNumber, FullName e Cpf à classe Customer

--- LearnShop.Model/Users/Customer.cs
+++ LearnShop.Model/Users/Customer.cs
@@ -5,4 +5,7 @@ namespace LearnShop.Model.Users;
 public class Customer : User
 {
     public List<Order> Orders { get; set; } = new List<Order>();
+    public string? PhoneNumber { get; set; }
+    public string FullName => $"{FirstName} {LastName}"; 
+    public string Cpf { get; set; } = string.Empty;
 }
