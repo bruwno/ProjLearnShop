982eb0d30ca95e7580d19ec02121aeb6e12e6eb0
William Bruno <william.silva@viannasempre.com>
2025-06-28 22:32:55 -0300

    Alteração a tabela sales para renomear a coluna price para total_price e implementação da SaleRespository

--- LearnShop.Infra/SaleRepository.cs
+++ LearnShop.Infra/SaleRepository.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using Dapper;
 using LearnShop.Infra.Interfaces;
 using LearnShop.Model.Sales;
 
@@ -10,28 +11,98 @@ public class SaleRepository : BaseRepository, ISaleRepository
     {
     }
 
-    public Task<IEnumerable<Sale?>> GetAllAsync()
+    public async Task<IEnumerable<Sale?>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return await ExecuteWithConnectionAsync(async connection =>
+        {
+            const string sql = @"SELECT id, 
+                                        sale_id, 
+                                        product_id, 
+                                        customer_id, 
+                                        quantity, 
+                                        total_price, 
+                                        sale_date
+                                 FROM sales";
+
+            return await connection.QueryAsync<Sale>(sql);
+        });
     }
 
-    public Task<Sale?> GetByIdAsync(long id)
+    public async Task<Sale?> GetByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        return await ExecuteWithConnectionAsync(async connection =>
+        {
+            const string sql = "SELECT * FROM sales WHERE id = @Id";
+            return await connection.QueryFirstOrDefaultAsync<Sale>(sql, new { Id = id });
+        });
     }
 
-    public Task<Sale?> InsertAsync(Sale entity)
+    public async Task<Sale?> InsertAsync(Sale sale)
     {
-        throw new NotImplementedException();
+        return await ExecuteWithConnectionAsync(async connection =>
+        {
+            const string sql = @"
+                INSERT INTO sales 
+                    (id, sale_id, product_id, customer_id, quantity, total_price, sale_date)
+                VALUES 
+                    (@Id, @SaleId, @ProductId, @CustomerId, @Quantity, @TotalPrice, @SaleDate)
+                RETURNING
+                    id, sale_id, product_id, customer_id, quantity, total_price, sale_date";
+
+            var parameters = new
+            {
+                sale.Id,
+                sale.SaleId,
+                sale.ProductId,
+                sale.CustomerId,
+                sale.Quantity,
+                sale.TotalPrice,
+                sale.SaleDate
+            };
+
+            var newSale = await connection.QuerySingleOrDefaultAsync<Sale>(sql, parameters);
+            return newSale;
+        });
     }
 
-    public Task<Sale?> UpdateAsync(long id, Sale entity)
+    public async Task<Sale?> UpdateAsync(long id, Sale sale)
     {
-        throw new NotImplementedException();
+        return await ExecuteWithConnectionAsync(async connection =>
+        {
+            const string sql = @"
+                UPDATE sales 
+                SET sale_id = @SaleId,
+                    product_id = @ProductId,
+                    customer_id = @CustomerId,
+                    quantity = @Quantity,
+                    total_price = @TotalPrice,
+                    sale_date = @SaleDate
+                WHERE id = @Id
+                RETURNING 
+                    id, sale_id, product_id, customer_id, quantity, total_price, sale_date";
+
+            var parameters = new
+            {
+                sale.SaleId,
+                sale.ProductId,
+                sale.CustomerId,
+                sale.Quantity,
+                sale.TotalPrice,
+                sale.SaleDate,
+                Id = id
+            };
+
+            await connection.ExecuteAsync(sql, parameters);
+            return sale;
+        });
     }
 
-    public Task DeleteAsync(long id)
+    public async Task DeleteAsync(long id)
     {
-        throw new NotImplementedException();
+        await ExecuteWithConnectionAsync(async connection =>
+        {
+            const string sql = "DELETE FROM sales WHERE id = @Id";
+            await connection.ExecuteAsync(sql, new { Id = id });
+        });
     }
 }
--- LearnShop.Model/Sales/Sale.cs
+++ LearnShop.Model/Sales/Sale.cs
@@ -7,7 +7,7 @@ public class Sale : BaseModel
     public long SaleId { get; set; }
     public long ProductId { get; set; }
     public int Quantity { get; set; }
-    public decimal Price { get; set; }
+    public decimal TotalPrice { get; set; }
     public DateTime SaleDate { get; set; }
     public long CustomerId { get; set; }
 }
