55b6319e6698bad72a20f4779469dbbdf3ecd368
Samuel Costa <samueljf18@hotmail.com>
2025-06-29 11:50:06 -0300

    Adicionando enum OrderStatus e refatorando a classe Order para suportar múltiplos itens e status do pedido

--- LearnShop.Model/Sales/Order.cs
+++ LearnShop.Model/Sales/Order.cs
@@ -1,3 +1,4 @@
+using LearnShop.Model.Enums;
 using LearnShop.Model.Products;
 using LearnShop.Model.Users;
 
@@ -6,7 +7,8 @@ namespace LearnShop.Model.Sales;
 public class Order : BaseModel
 {
     public long CustomerId { get; set; }
-    public OrderItem OrderItem { get; set; }
+    public List<OrderItem> Items { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public OrderStatus Status { get; set; }
     public decimal TotalPrice { get; set; } = 0.0m;
 }
