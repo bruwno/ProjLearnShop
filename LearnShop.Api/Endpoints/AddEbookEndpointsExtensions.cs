9399762e7f7d5b1d47ad02ed579faea274891ad7
Mathews <baiomathews@gmail.com>
2025-06-30 23:03:42 -0300

    Endpoints de ebook finalizados

--- LearnShop.Api/Endpoints/AddEbookEndpointsExtensions.cs
+++ LearnShop.Api/Endpoints/AddEbookEndpointsExtensions.cs
@@ -9,24 +9,20 @@ public static class AddEbookEndpointsExtensions
     {
         var ebooks = app.MapGroup("/ebooks");
 
-        ebooks.MapPost("/", CreateEbook);
+        ebooks.MapGet("/", GetAllEbooks);
         ebooks.MapGet("/{id:long}", GetEbookById);
+        ebooks.MapGet("/categoria/{categoria}", GetEbooksByCategory);
+        ebooks.MapPost("/", CreateEbook);
+        ebooks.MapPut("/{id:long}", UpdateEbook);
+        ebooks.MapDelete("/{id:long}", DeleteEbook);
     }
 
-    private static async Task<IResult> CreateEbook(Ebook ebook, IEbookService ebookService)
+    private static async Task<IResult> GetAllEbooks(IEbookService ebookService)
     {
         try
         {
-            var created = await ebookService.CreateEbookAsync(ebook);
-            return TypedResults.Created($"/ebooks/{created?.Id}", created);
-        }
-        catch (ArgumentNullException ex)
-        {
-            return TypedResults.BadRequest(ex.Message);
-        }
-        catch (ArgumentException ex)
-        {
-            return TypedResults.BadRequest(ex.Message);
+            var ebooks = await ebookService.GetEbooksAsync();
+            return TypedResults.Ok(ebooks);
         }
         catch (Exception ex)
         {
@@ -51,5 +47,76 @@ public static class AddEbookEndpointsExtensions
             return TypedResults.InternalServerError($"Ocorreu um erro interno: {ex.Message}");
         }
     }
-}
 
+    private static async Task<IResult> GetEbooksByCategory(string categoria, IEbookService ebookService)
+    {
+        try
+        {
+            var ebooks = await ebookService.GetEbooksByCategoryAsync(categoria);
+            return TypedResults.Ok(ebooks);
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.InternalServerError($"Ocorreu um erro interno: {ex.Message}");
+        }
+    }
+
+    private static async Task<IResult> CreateEbook(Ebook ebook, IEbookService ebookService)
+    {
+        try
+        {
+            var created = await ebookService.CreateEbookAsync(ebook);
+            return TypedResults.Created($"/ebooks/{created?.Id}", created);
+        }
+        catch (ArgumentNullException ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+        catch (ArgumentException ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.InternalServerError($"Ocorreu um erro interno: {ex.Message}");
+        }
+    }
+
+    private static async Task<IResult> UpdateEbook(long id, Ebook ebook, IEbookService ebookService)
+    {
+        try
+        {
+            var updated = await ebookService.UpdateEbookAsync(id, ebook);
+            return TypedResults.Ok(updated);
+        }
+        catch (ArgumentNullException ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+        catch (ArgumentException ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.InternalServerError($"Ocorreu um erro interno: {ex.Message}");
+        }
+    }
+
+    private static async Task<IResult> DeleteEbook(long id, IEbookService ebookService)
+    {
+        try
+        {
+            await ebookService.DeleteEbookAsync(id);
+            return TypedResults.NoContent();
+        }
+        catch (ArgumentException ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.InternalServerError($"Ocorreu um erro interno: {ex.Message}");
+        }
+    }
+}
